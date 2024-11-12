18cf13f3756da1b4f8f1154bea1d0b8d386e0984
Florent Fougères <florent.fougeres@gmail.com>
2024-11-12 20:13:10 +0100

    timemanager update

--- src/timemanager.py
+++ src/timemanager.py
@@ -1,19 +1,24 @@
 from datetime import datetime, timedelta
 
 update_hours = ["020000", "060000", "100000", "140000", "180000", "220000"]
-
-# Le vrai départ est le 10/11/2024 mais les pointages le premier jour ne sont pas réguliers on va les gérer à part
-start_date = datetime(2024, 11, 11)
-
-hours_exception = ["1304000"]
+hours_exception = "130400"
 
 def list_of_dates_between_today_and_departure():
     """
     Génère une liste de toutes les dates entre la date actuelle et la date du départ, au format AAAAMMJJ.
     """
-    delta = (start_date - datetime.now()).days
-    dates = [(datetime.now() + timedelta(days=i)).strftime('%Y%m%d') for i in range(delta + 1)]
+    # Le vrai départ est le 10/11/2024 mais les pointages le premier jour ne sont pas réguliers on va les gérer à part
+    start_date = datetime(2024, 11, 11)
+
+    delta = (datetime.now() - start_date).days
+    dates = []
+
+    for i in range(delta + 1):
+        current_date = start_date + timedelta(days=i)
+        dates.append(current_date.strftime('%Y%m%d'))
+    
     return dates
+
 
 def get_last_update():
     """Renvoie l'heure du dernier pointage la plus proche de l'heure actuelle.
@@ -32,3 +37,5 @@ def get_last_update():
 
 def get_current_date():
     return datetime.now().strftime("%Y%m%d")
+
+print(list_of_dates_between_today_and_departure())
