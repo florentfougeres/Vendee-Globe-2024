98ed69b799e6cc690ffaccc2bee7d54077c4a593
Florent Fougères <florent.fougeres@gmail.com>
2024-11-14 18:35:50 +0100

    Ajout de docstring et typing

--- src/processor.py
+++ src/processor.py
@@ -4,16 +4,39 @@ from shapely.geometry import Point, LineString
 from pathlib import Path
 from datetime import datetime
 import re
+import logging
 
+def parse_coordinates(coord: str) -> tuple[int, int, int, str]:
+    """Analyse une chaîne de caractères représentant une coordonnée 
+    en degrés, minutes, secondes et direction.
 
-def dms_to_decimal(degrees, minutes, seconds, direction):
-    "Convertir des coordonnées DMS en degrés décimaux"
-    degrees = degrees.strip()
-    minutes = minutes.strip().replace("'", "")
-    seconds = seconds.strip().replace(
-        "'",
-        "",
-    )
+    Args:
+        coord (str): Coordonnée en format 'DD°MM.SS[D]' où D est la 
+        direction (N/S/E/W).
+
+    Returns:
+        tuple[int, int, int, str]: Un tuple contenant les degrés (int), 
+        minutes (int), secondes (int), et la direction (str : 'N', 'S', 'E' ou 'W').
+    """
+    direction = coord[-1]  # Dernier caractère (N/S/E/W)
+    parts = coord[:-1].replace("'", "").replace('"', "").split("°")
+    degrees = parts[0].strip()
+    minutes, seconds = parts[1].split(".")
+
+    return int(degrees), int(minutes), int(seconds), direction
+
+def dms_to_decimal(degrees: int, minutes: int, seconds: int, direction: str) -> float:
+    """Convertir des degrés minutes secondes en degrés décimaux
+
+    Args:
+        degrees (int): Degrés
+        minutes (int): Minutes
+        seconds (int): Secondes
+        direction (str): Direction (N / S / E / O)
+
+    Returns:
+        float: Coordordonnées décimales
+    """
 
     decimal = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
 
@@ -22,7 +45,20 @@ def dms_to_decimal(degrees, minutes, seconds, direction):
     return decimal
 
 
-def parse_hour(hour_str, date_obj):
+def parse_hour(hour_str: str, date_obj: datetime) -> datetime | None:
+    """Parse une chaîne de caractères représentant une heure et combine avec une date 
+    pour créer un objet datetime.
+
+    Args:
+        hour_str (str): Chaîne représentant l'heure, au format 'HH:MM' avec éventuellement 
+                        le suffixe ' FR'.
+        date_obj (datetime): Objet datetime représentant la date à laquelle l'heure sera 
+                             combinée.
+
+    Returns:
+        datetime | None: Un objet datetime combinant la date et l'heure si l'heure est valide, 
+                         sinon None.
+    """
     if pd.isnull(hour_str):
         return None
 
@@ -33,31 +69,24 @@ def parse_hour(hour_str, date_obj):
         )
     return None
 
+def read_xlsx(file: Path, param_date: str) -> pd.DataFrame:
+    """Charge un fichier Excel dans un DataFrame sans en-têtes et réorganise les colonnes.
 
-def read_xlsx(file: Path, param_date: str):
-    """Charger le excel dans un dataframe sans en-têtes et réorganiser les colonnes"""
-    # Définir les noms de colonnes dans l"ordre souhaité
+    Args:
+        file (Path): Chemin vers le fichier Excel.
+        param_date (str): Date en format 'YYYYMMDD' utilisée pour associer un timestamp 
+                          aux heures du fichier.
+
+    Returns:
+        pd.DataFrame: DataFrame contenant les données chargées et nettoyées, avec une colonne 
+                      timestamp et les colonnes skipper et bateau séparées.
+    """
+    # Définir les noms de colonnes dans l'ordre souhaité
     columns = [
-        "rang",
-        "code",
-        "nom",
-        "heure",
-        "latitude",
-        "longitude",
-        "30m_cap",
-        "30m_vitesse",
-        "30m_vmg",
-        "30m_distance",
-        "last_rank_cap",
-        "last_rank_vitesse",
-        "last_rank_vmg",
-        "last_rank_distance",
-        "24h_cap",
-        "24h_vitesse",
-        "24h_vmg",
-        "24h_distance",
-        "dtf",
-        "dtl",
+        "rang", "code", "nom", "heure", "latitude", "longitude", "30m_cap", 
+        "30m_vitesse", "30m_vmg", "30m_distance", "last_rank_cap", 
+        "last_rank_vitesse", "last_rank_vmg", "last_rank_distance", 
+        "24h_cap", "24h_vitesse", "24h_vmg", "24h_distance", "dtf", "dtl"
     ]
 
     df = pd.read_excel(
@@ -69,10 +98,10 @@ def read_xlsx(file: Path, param_date: str):
         names=columns,
         engine="calamine",
     )
+
+    # Nettoyage des sauts de ligne dans les chaînes
     df = df.apply(
-        lambda col: col.map(
-            lambda x: x.replace("\r\n", " - ") if isinstance(x, str) else x
-        )
+        lambda col: col.map(lambda x: x.replace("\r\n", " - ") if isinstance(x, str) else x)
     )
 
     date_obj = datetime.strptime(param_date, "%Y%m%d")
@@ -81,29 +110,32 @@ def read_xlsx(file: Path, param_date: str):
 
     return df
 
+def build_gpkg(file: Path, name: str, date: str, output_dir: Path = Path("./")) -> None:
+    """Construire un fichier GeoPackage (.gpkg) à partir d'un fichier Excel et d'une date.
 
-def parse_coordinates(coord):
-    direction = coord[-1]  # Dernier caractère (N/S/E/W)
-    parts = coord[:-1].split(
-        "°"
-    )  # On retire la direction et on sépare les degrés des minutes
-    degrees = parts[0]
-    minutes, seconds = parts[1].split(".")
-    return degrees, minutes, seconds, direction
-
-
-def build_gpkg(file: Path, name: str, date: str, output_dir: Path = Path("./")):
+    Args:
+        file (Path): Chemin vers le fichier Excel contenant les données.
+        name (str): Nom de base pour le fichier GeoPackage de sortie.
+        date (str): Date utilisée pour générer les coordonnées de timestamp.
+        output_dir (Path, optional): Répertoire où sauvegarder le fichier .gpkg. Par défaut, 
+                                     le répertoire courant.
+    """
     gdf = create_geom(file, date)
     output_path = output_dir / f"{name}.gpkg"
-
     gdf.to_file(output_path, driver="GPKG")
 
 
-def create_geom(file, date):
-    """Créer un geodataframe avec la colonne de géometrie à partir du read excel
+def create_geom(file: Path, date: str) -> gpd.GeoDataFrame:
+    """Créer un GeoDataFrame avec une colonne de géométrie à partir des données Excel.
+
+    Args:
+        file (Path): Chemin vers le fichier Excel contenant les données.
+        date (str): Date utilisée pour générer les coordonnées de timestamp.
+
+    Returns:
+        gpd.GeoDataFrame: Un GeoDataFrame avec la géométrie et les données associées.
     """
     df = read_xlsx(file, date)
-
     df["latitude_decimal"] = df["latitude"].apply(
         lambda x: dms_to_decimal(*parse_coordinates(x))
     )
@@ -120,40 +152,53 @@ def create_geom(file, date):
     return gdf
 
 
-def aggreger_geodataframes(liste_geodfs: list):
-    """
-    Agrège une liste de GeoDataFrames en un seul GeoDataFrame.
-    """
+def aggreger_geodataframes(liste_geodfs: list[gpd.GeoDataFrame]) -> gpd.GeoDataFrame:
+    """Agrège une liste de GeoDataFrames en un seul GeoDataFrame.
 
+    Args:
+        liste_geodfs (list): Liste de GeoDataFrames à combiner.
+
+    Returns:
+        gpd.GeoDataFrame: GeoDataFrame résultant de l'agrégation.
+    
+    Raises:
+        ValueError: Si la liste de GeoDataFrames est vide.
+    """
     if not liste_geodfs:
         raise ValueError("La liste de GeoDataFrames est vide.")
 
     geodf_agrege = gpd.GeoDataFrame(pd.concat(liste_geodfs, ignore_index=True))
-
     return geodf_agrege
 
-def create_trejectoire(gdf):
+
+def create_trejectoire(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Crée une trajectoire pour chaque code en regroupant les points par code et en créant 
+    une `LineString`.
+
+    Args:
+        gdf (gpd.GeoDataFrame): Le GeoDataFrame contenant les données à regrouper.
+
+    Returns:
+        gpd.GeoDataFrame: Un GeoDataFrame contenant des `LineString` pour chaque code.
+    """
     gdf = gdf.sort_values(by=['code', 'timestamp'])
-
     trajectories = gdf.groupby('code').apply(lambda x: LineString(x.geometry.tolist()))
-
     trajectory_gdf = gpd.GeoDataFrame(trajectories, columns=['geometry'], crs=gdf.crs).reset_index()
-
     return trajectory_gdf
 
-def export_to_gpkg(gdf, filepath, layer_name="layer"):
-    """
-    Exporte un GeoDataFrame dans un fichier GeoPackage (.gpkg).
-    
-    Paramètres :
-    - gdf (GeoDataFrame) : le GeoDataFrame à exporter.
-    - filepath (str) : le chemin complet du fichier GeoPackage de sortie (incluant l'extension .gpkg).
-    - layer_name (str) : nom de la couche dans le GeoPackage (par défaut 'layer').
 
+def export_to_gpkg(gdf: gpd.GeoDataFrame, filepath: Path, layer_name: str = "layer") -> None:
+    """Exporte un GeoDataFrame dans un fichier GeoPackage (.gpkg).
+
+    Args:
+        gdf (gpd.GeoDataFrame): Le GeoDataFrame à exporter.
+        filepath (Path): Le chemin du fichier GeoPackage de sortie.
+        layer_name (str, optional): Le nom de la couche dans le GeoPackage. Par défaut 'layer'.
+    
     """
     try:
         # Exporter vers un fichier GeoPackage
         gdf.to_file(filepath, layer=layer_name, driver="GPKG")
-        print(f"Exportation réussie vers {filepath} dans la couche '{layer_name}'.")
+        logging.info(f"Exportation réussie vers {filepath} dans la couche '{layer_name}'.")
     except Exception as e:
-        print(f"Erreur lors de l'exportation : {e}")
+        logging.error(f"Erreur lors de l'exportation : {e}")
