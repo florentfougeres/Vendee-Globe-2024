49e91b5321fc5239ec58294162a49b236223e1c4
Florent Fougères <florent.fougeres@gmail.com>
2024-11-14 18:53:04 +0100

    Docstring et remise en forme du code

--- dernier_pointage.py
+++ dernier_pointage.py
@@ -1,16 +1,27 @@
+"Dernier pointage en date"
+
 import argparse
+from pathlib import Path
 from src.downloader import download_file, build_url
 from src.processor import build_gpkg
 from src.timemanager import get_current_date, get_last_update
-from pathlib import Path
+
 
 def main():
+    """
+    Point d'entrée principal du programme.
+
+    Cette fonction parse les arguments de la ligne de commande, obtient la dernière mise à jour
+    et la date actuelle, construit l'URL pour télécharger le fichier, puis appelle les fonctions
+    pour télécharger et traiter les données. Enfin, elle génère un fichier GPKG dans le répertoire
+    spécifié ou dans le répertoire par défaut.
+    """
     parser = argparse.ArgumentParser(description="Télécharge et traite les données.")
     parser.add_argument(
-        '--output-dir', 
-        type=Path, 
-        default=Path("./"), 
-        help="Répertoire d'export pour le fichier GPKG"
+        "--output-dir",
+        type=Path,
+        default=Path("./"),
+        help="Répertoire d'export pour le fichier GPKG",
     )
     args = parser.parse_args()
 
@@ -19,7 +30,13 @@ def main():
     url = build_url(date, last_update)
     file = f"./.data/data_{date}_{last_update}.xlsx"
     download_file(url, file)
-    build_gpkg(Path(file), name=f"classement_{date}_{last_update}", date=date, output_dir=args.output_dir)
+    build_gpkg(
+        Path(file),
+        name=f"classement_{date}_{last_update}",
+        date=date,
+        output_dir=args.output_dir,
+    )
+
 
 if __name__ == "__main__":
     main()
--- trajectoires_pointages.py
+++ trajectoires_pointages.py
@@ -1,41 +1,62 @@
+"Intégralité des pointages depuis le départ et trajectoire."
 import argparse
 import logging
-from src.downloader import download_file, build_url
-from src.processor import build_gpkg, create_geom, aggreger_geodataframes, create_trejectoire, export_to_gpkg
-from src.timemanager import update_hours, list_of_dates_between_today_and_departure, hours_exception
 from pathlib import Path
 import re
+from src.downloader import download_file, build_url
+from src.processor import (
+    build_gpkg,
+    create_geom,
+    aggreger_geodataframes,
+    create_trejectoire,
+    export_to_gpkg,
+)
+from src.timemanager import (
+    update_hours,
+    list_of_dates_between_today_and_departure,
+    hours_exception,
+)
+
 
 def main():
+    """
+    Point d'entrée principal du programme.
+
+    Cette fonction parse les arguments de la ligne de commande, configure la gestion des logs,
+    télécharge les fichiers de données pour les différentes dates et heures, crée les géométries,
+    agrège les données et exporte les résultats sous forme de fichiers GPKG pour les pointages
+    et les trajectoires.
+    """
     parser = argparse.ArgumentParser(description="Télécharge et traite les données.")
     parser.add_argument(
-        '-o', '--output-dir', 
-        type=Path, 
-        default=Path("./"), 
-        help="Répertoire d'export pour le fichier GPKG"
+        "-o",
+        "--output-dir",
+        type=Path,
+        default=Path("./"),
+        help="Répertoire d'export pour le fichier GPKG",
     )
     parser.add_argument(
-        '-v', '--verbose', 
-        action='store_true', 
-        help="Active le mode verbose pour plus de détails"
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Active le mode verbose pour plus de détails",
     )
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
     all_dates = list_of_dates_between_today_and_departure()
-    
+
     list_url = []
     list_file = []
-
 
     url_premier_jour = build_url("20241110", hours_exception)
     list_url.append(url_premier_jour)
     file_premier_jour = f"./.data/data_20241110_{hours_exception}.xlsx"
     list_file.append(file_premier_jour)
 
-    for date in all_dates: 
-        for hours in update_hours: 
+    for date in all_dates:
+        for hours in update_hours:
             url = build_url(date, hours)
             list_url.append(url)
             file = f"./.data/data_{date}_{hours}.xlsx"
@@ -49,19 +70,21 @@ def main():
             logging.info(f"Le fichier {file} existe déjà, téléchargement ignoré.")
 
     list_gdf = []
-    for file in list_file :
+    for file in list_file:
         if Path(file).exists():
-            date = re.search(r"data_(\d{8})_", file).group(1) if re.search(r"data_(\d{8})_", file) else None
+            date = (
+                re.search(r"data_(\d{8})_", file).group(1)
+                if re.search(r"data_(\d{8})_", file)
+                else None
+            )
             gdf = create_geom(file, date)
             list_gdf.append(gdf)
 
     final_gdf = create_trejectoire(aggreger_geodataframes(list_gdf))
 
-    export_to_gpkg(aggreger_geodataframes(list_gdf), Path(args.output_dir / f"data_{date}.gpkg"), "pointages" )
-    export_to_gpkg(final_gdf, Path(args.output_dir / f"data_{date}.gpkg"), "trajectoire" )
+    export_to_gpkg(aggreger_geodataframes(list_gdf), Path(args.output_dir / f"data_{date}.gpkg"), "pointages")
+    export_to_gpkg(final_gdf, Path(args.output_dir / f"data_{date}.gpkg"), "trajectoire")
 
-
-        
 
 if __name__ == "__main__":
     main()
